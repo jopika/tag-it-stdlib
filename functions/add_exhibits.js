db65d180dbe69c5a3e119bac8dfbe696df9be30f
Tiger Oakes <tigeroakes@gmail.com>
2018-01-13 22:17:52 -0800

    Added exhibits

---
+++ functions/add_exhibits.js
@@ -0,0 +1,39 @@
+const { MongoClient } = require("mongodb");
+
+let db = null;
+
+/**
+ * Add exhibits
+ * @param {object} list
+ * @returns {object} insertedIds
+ */
+module.exports = async function add_exhibits(list, context) {
+	let uri = process.env["MONGO_URI"];
+
+	// Load database unless cached
+	if (db == null) {
+		// (async) obtain an instance of the client
+		const client = await MongoClient.connect(uri);
+		// connect to the database
+		db = client.db("tagit");
+	}
+
+	// set tag and name for user, upsert/create if does not exist
+	const r = await db.collection("exhibit").insertMany(
+		Object.keys(list).map(key => {
+			let collectibles = {};
+			for (const bit of Object.keys(list[key].collectibles)) {
+				const num = Number(bit);
+				collectibles[num] = list[key].collectibles[bit];
+			}
+
+			return {
+				key,
+				name: list[key].name,
+				photoUrl: list[key].photoUrl,
+				collectibles
+			};
+		})
+	);
+	return r.insertedIds;
+};
