e650dbba76cafabfdd2b2c3fb6ecad93a436ce01
jopika <jojo12345678@gmail.com>
2018-01-13 22:50:27 -0800

    Update function slightly

--- functions/tagged.js
+++ functions/tagged.js
@@ -26,18 +26,15 @@ module.exports = async function tagged(userTag, targetTag, context) {
     let userObj     = userTable.findone({"id":userTag});
     let targetObj   = exhibitTable.findone({"id":targetTag});
 
-    // 
     let userInventory   = userObj.inventory;
+
     // TODO: Check this
     let targetCollection = targetObj.collection; 
-    
     let sizeOfCollection = Object.keys(targetCollection).length;
-
-    let randomNumber = Math.floor((Math.random() * 100)) % sizeOfCollection
-
+    let randomNumber = Math.floor((Math.random() * 100)) % sizeOfCollection;
     let item = targetCollection[randomNumber];
 
-    userObj.userObj.inventory.push(item)
+    userInventory.push(item)
 
     userTable.updateOne({"id":userTag}, 
         {$set: {"inventory": userInventory}});
@@ -46,17 +43,4 @@ module.exports = async function tagged(userTag, targetTag, context) {
      targetTag, item, context);
 
 
-
-
-    // let newItem         =
-
-/*    if(targetObj.supertag) {
-        // TODO: Clone an item from the
-    } else {
-        // TODO: Select a 'random' item in the pocket
-        // NOTE: We 'should' not
-        if(STEAL_BOOL) {
-
-        }
-    }*/
 }
