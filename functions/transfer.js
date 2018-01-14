b143b3986ab135881507cbb93d084e1cbbb1c2db
Tiger Oakes <tigeroakes@gmail.com>
2018-01-13 21:59:19 -0800

    Add docs to transfer

--- functions/transfer.js
+++ functions/transfer.js
@@ -2,31 +2,29 @@ const { MongoClient } = require("mongodb");
 
 let db = null;
 
-/** 
+/**
  * Transfers an item from target to user
- * 
+ * @param {string} userTag
+ * @param {string} targetTag
+ * @returns {any}
  */
-
 module.exports = async function transfer(userTag, targetTag, context) {
-    let uri = process.env["MONGO_URI"];
+	let uri = process.env["MONGO_URI"];
 
-    if (db == null) {
-        const client = await MongoClient.connect(uri);
-        db = client.db("tagit");
-    }
+	if (db == null) {
+		const client = await MongoClient.connect(uri);
+		db = client.db("tagit");
+	}
 
-    let userTable = db.collection("user");
+	let userTable = db.collection("user");
 
-    let userObj     = userTable.findone({userTag});
-    let targetObj   = userTable.findone({targetTag});
+	let userObj = userTable.findone({ userTag });
+	let targetObj = userTable.findone({ targetTag });
 
-    if(targetObj.supertag) {
-        // TODO: Clone an item  
-        
-    } else {
-        // TODO: Select a 'random' item in the pocket
-        // NOTE: We 'should' not
-
-    }
-
-}
+	if (targetObj.supertag) {
+		// TODO: Clone an item
+	} else {
+		// TODO: Select a 'random' item in the pocket
+		// NOTE: We 'should' not
+	}
+};
