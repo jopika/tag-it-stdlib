658dc44ec545148b58354d52ec164c1d3b3030a7
Tiger Oakes <tigeroakes@gmail.com>
2018-01-14 11:37:51 -0800

    fixed get inventory

--- functions/get_inventory.js
+++ functions/get_inventory.js
@@ -3,30 +3,21 @@ const { MongoClient } = require("mongodb");
 let db = null;
 
 /**
- * Log a transaction
- * @param {string} user_id
- * @returns {array} ok
+ * Return list of item names in user's inventory
+ * @param {string} user_id tag of user
+ * @returns {array} string list
  */
+module.exports = async function get_inventory(user_id, context) {
+	let uri = process.env["MONGO_URI"];
 
- module.exports = async function get_inventory(
-    user_id,
-    context) {
-    let uri = process.env["MONGO_URI"];
+	if (db == null) {
+		const client = await MongoClient.connect(uri);
+		db = client.db("tagit");
+	}
 
-    if (db == null) {
-        const client = await MongoClient.connect(uri);
-        db = client.db("tagit");
-    }
+	let userTable = db.collection("user");
+	let userObj = await userTable.findOne({ tag: user_id });
+	let inventory = userObj.inventory;
 
-    let userTable = db.collection("user");
-    let userObj = userTable.findOne({"tag":user_id});
-    let inventory = userObj.inventory;
-
-    let array = new Array(inventory.length);
-
-    for (var i = inventory.length - 1; i >= 0; i--) {
-        array.push(inventory.get(i));
-    }
-
-    return array;
- }
+	let array = userObj.inventory.map(item => item.name);
+};
