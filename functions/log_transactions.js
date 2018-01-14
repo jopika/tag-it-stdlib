e4f5e5acb8876bbcb698fabc37014ebed68d2cd4
jacketsj <jacketsj@gmail.com>
2018-01-13 23:02:54 -0800

    ayy

--- functions/log_transactions.js
+++ functions/log_transactions.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require("mongodb");
+var request = require('request');
 
 let db = null;
 
@@ -35,5 +36,26 @@ module.exports = async function log_transaction(
 	};
 
 	const r = await db.collection("transaction").insertOne(transaction);
+
+	request.post(
+		'https://hooks.slack.com/services/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'
+		{
+			"access_token": "xoxp-XXXXXXXX-XXXXXXXX-XXXXX",
+				"scope": "incoming-webhook",
+				"team_name": "Team Installing Your Hook",
+				"incoming_webhook": {
+					"url": "https://hooks.slack.com/TXXXXX/BXXXXX/XXXXXXXXXX",
+						"channel": "#channel-it-will-post-to",
+						"channel_id": "C05002EAE",
+						"configuration_url": "https://teamname.slack.com/services/BXXXXX"
+					        }
+		}
+		function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				console.log(body);
+			}
+		}
+	);
+
 	return r.result.ok;
 };
